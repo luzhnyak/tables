cd9bed1ea19c7d5ab4d88c09997b5e42f7104e67
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-16 19:42:07 +0200

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Table from "./components/Table/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+    </div>
+  );
+}
+
+export default App;
